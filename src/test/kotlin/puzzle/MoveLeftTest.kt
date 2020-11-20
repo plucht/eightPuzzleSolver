efc2c6c6ff683cafb6206596a4f94ed9930aca00
Peter Lucht <hape@quantenparallaxum.de>
2020-11-20 18:42:54 +0100

    inline intermediate scope variables

--- src/test/kotlin/puzzle/MoveLeftTest.kt
+++ src/test/kotlin/puzzle/MoveLeftTest.kt
@@ -8,15 +8,10 @@ internal class MoveLeftTest {
     private val EMPTY_CELL_IS_LEFT = arrayOf(1, 2, 3, 4, 5, 6, 0, 7, 8)
 
     @Test fun testMoveEmptyCellLeft() {
-        val currentState = EMPTY_CELL_IS_CENTERED
-        val expectedState = EMPTY_CELL_IS_LEFT
-
-        assertArrayEquals(expectedState, MoveLeft().move(currentState))
+        assertArrayEquals(EMPTY_CELL_IS_LEFT, MoveLeft().move(EMPTY_CELL_IS_CENTERED))
     }
 
     @Test fun testDoNothingWhenEmptyCellIsFarLeft() {
-        val currentState = EMPTY_CELL_IS_LEFT
-
-        assertArrayEquals(currentState, MoveLeft().move(currentState))
+        assertArrayEquals(EMPTY_CELL_IS_LEFT, MoveLeft().move(EMPTY_CELL_IS_LEFT))
     }
 }
--- src/test/kotlin/puzzle/MoveRightTest.kt
+++ src/test/kotlin/puzzle/MoveRightTest.kt
@@ -8,15 +8,10 @@ internal class MoveRightTest {
     private val EMPTY_CELL_IS_RIGHT = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 0)
 
     @Test fun testMoveEmptyCellRight() {
-        val currentState = EMPTY_CELL_IS_CENTERED
-        val expectedState = EMPTY_CELL_IS_RIGHT
-
-        assertArrayEquals(expectedState, MoveRight().move(currentState))
+        assertArrayEquals(EMPTY_CELL_IS_RIGHT, MoveRight().move(EMPTY_CELL_IS_CENTERED))
     }
 
     @Test fun testDoNothingWhenEmptyCellIsFarRight() {
-        val currentState = EMPTY_CELL_IS_RIGHT
-
-        assertArrayEquals(currentState, MoveRight().move(currentState))
+        assertArrayEquals(EMPTY_CELL_IS_RIGHT, MoveRight().move(EMPTY_CELL_IS_RIGHT))
     }
 }
